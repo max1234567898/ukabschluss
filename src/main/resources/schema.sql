5c064193ae461d3b2f4eee2a77b5902f37581dd0
Max.Ludwig <max.ludwig@lernende.bbw.ch>
2022-09-19 11:10:10 +0200

    created schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,34 @@
-DROP TABLE IF EXISTS CATEGORY CASCADE;
-CREATE TABLE CATEGORY
-(
-    id   UUID,
-    name VARCHAR(50) NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS GAME CASCADE;
-CREATE TABLE GAME
-(
-    id       UUID,
-    name     VARCHAR(50) NOT NULL,
-    category UUID NOT NULL,
-
-    PRIMARY KEY (id),
-    FOREIGN KEY (category) REFERENCES CATEGORY (id)
-);
-
 DROP TABLE IF EXISTS MEMBER CASCADE;
 CREATE TABLE MEMBER
 (
-    id            UUID,
-    username      VARCHAR(2000) NOT NULL,
-    password_hash VARCHAR(2000) NOT NULL,
-    is_admin      BOOLEAN       NOT NULL DEFAULT FALSE,
+    id        UUID,
+    firstname VARCHAR(200) NOT NULL,
+    lastname  VARCHAR(200) NOT NULL,
+    email     VARCHAR(200) NOT NULL,
+    password  VARCHAR(200) NOT NULL,
+    is_admin  BOOLEAN      NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id)
+);
 
+DROP TABLE IF EXISTS PLACE CASCADE;
+CREATE TABLE PLACE
+(
+    id       UUID,
+    location VARCHAR(200),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS BOOKING CASCADE;
+CREATE TABLE BOOKING
+(
+    id          UUID,
+    startDate   TIMESTAMP NOT NULL,
+    endDate     TIMESTAMP NOT NULL,
+    is_accepted BOOLEAN   NOT NULL DEFAULT FALSE,
+    member_id   UUID,
+    place_id    UUID,
+
+    FOREIGN KEY (member_id) REFERENCES MEMBER (id),
+    FOREIGN KEY (place_id) REFERENCES PLACE (id),
     PRIMARY KEY (id)
 );
