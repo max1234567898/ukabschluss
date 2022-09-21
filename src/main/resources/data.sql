fad5392ba35aeab5eee46db8a69106487d785b7f
Max.Ludwig <max.ludwig@lernende.bbw.ch>
2022-09-21 11:15:33 +0200

    Added Client requests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,9 @@ VALUES ('9135f11e-1b66-4ee6-bbae-df37303cc154', 'KK', 'kouroms','mitglied', '$2a
 
 INSERT INTO PLACE (id, location)
 VALUES ('4be5f5bf-8eb5-44ea-8eb5-a5e807856d09', 'Tisch 1'),
-       ('e1eec954-6ef4-4926-8183-7575af189f2a', 'Meetingraum 1');
+       ('e1eec954-6ef4-4926-8183-7575af189f2a', 'Meetingraum 1'),
+       ('e1eec954-6ef4-4926-8183-7575af189f2b', 'Meetingraum 3');
+
 
 INSERT INTO BOOKING (id, start_date, end_date, is_accepted, member_id, place_id)
 VALUES ('413e2297-b84b-42ef-97ed-16a8a9d1d671', '2012-09-17 18:47:52.069','2012-09-18 18:47:52.069',true,'9135f12e-1b66-4ee6-bbae-df37303cc154', '4be5f5bf-8eb5-44ea-8eb5-a5e807856d09'),
